d073662cd997c85957d63ffe796aed9983674d2e
Juveriya Fatima <132326573+Juveriyaae@users.noreply.github.com>
2023-12-03 18:36:51 -0600

    Create chisquare.py

---
+++ river/stats/chisquare.py
@@ -0,0 +1,32 @@
+from __future__ import annotations
+import collections
+from collections import defaultdict
+import numbers
+from river import base, stats 
+from scipy.stats import chi2_contingency
+class ChiSquare(base.Transformer):
+    def __init__(self):
+        #initlize chi-sq test.
+        self.observed_frequencies = defaultdict(lambda: defaultdict(int))
+        self.total_samples = 0
+        print("initalisation works!!")
+    def update(self, x, y):
+        # Updates observed & expected frequency based on the incoming data (x, y)
+        self.observed_frequencies[x][y] += 1
+        self.total_samples += 1
+        print("Update func works!!!")
+    def chi_squared_test(self):
+        observed_matrix = [
+            [self.observed_frequencies[x][y] for y in self.observed_frequencies[x]]
+            for x in self.observed_frequencies] 
+        _, p_value, _, _ = chi2_contingency(observed_matrix)
+        return p_value
+    def transform_one(self, x):
+        # Return the p-value for the chi-squared test
+        return self.chi_squared_test()
+    def transform_many(self, X):
+        return [self.transform_one(x) for x in X]
+    def reset(self):
+        self.observed_frequencies = defaultdict(lambda: defaultdict(int))
+        self.total_samples = 0
+
